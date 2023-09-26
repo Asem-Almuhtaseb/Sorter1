daa7804116eadcac1e6050e743ca93e184d56800
Asem-Almuhtaseb <121949995+Asem-Almuhtaseb@users.noreply.github.com>
2023-09-26 23:26:47 +0300

    Add files via upload
    Final version, after editing

--- Sorter.java
+++ Sorter.java
@@ -1,22 +1,44 @@
+import java.util.Arrays;
+
 public class Sorter {
-    public static void selectionSort(int[] arr){
-        for (int i = 0; i < arr.length - 1; i++)
-        {
+    public static void selectionSort(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
             int index = i;
-            for (int j = i + 1; j < arr.length; j++){
-                if (arr[j] < arr[index]){
-                    index = j;//searching for lowest index
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < array[index]) {
+                    index = j;
                 }
             }
-            int smallerNumber = arr[index];
-            arr[index] = arr[i];
-            arr[i] = smallerNumber;
+            int smallerNumber = array[index];
+            array[index] = array[i];
+            array[i] = smallerNumber;
         }
     }
 
-    public static void main(String[] args) {
-
-
+    public static void insertionSort(int[] array) {
+        for (int k = 1; k < array.length; k++) {
+            int temp = array[k];
+            int j = k - 1;
+            while (j >= 0 && temp <= array[j]) {
+                array[j + 1] = array[j];
+                j = j - 1;
+            }
+            array[j + 1] = temp;
+        }
     }
 
+    public static void sort(int[] array) {
+        insertionSort(array);
+    }
+    public static void sort2(int[] array) {
+        selectionSort(array);
+    }
+
+    public static void main(String[] args) {
+        int[] array = { 5, 3, 7, 2, 8, 4 };
+        sort(array);
+        System.out.println("Sorted array using insertionSort sort: " + Arrays.toString(array));
+        sort2(array);
+        System.out.println("Sorted array using selection sort: " + Arrays.toString(array));
+    }
 }
